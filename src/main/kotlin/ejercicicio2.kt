e65613837fb7da8a1ad0fb127ea421a024e4ece1
britza <bzegarraflores@danielcastelao.org>
2021-05-14 10:15:29 +0200

    segundo ejercicio resubido

--- src/main/kotlin/ejercicicio2.kt
+++ src/main/kotlin/ejercicicio2.kt
@@ -1,4 +1,5 @@
 class ejercicicio2 (var tamaño:Int, var rellenamiento:(Int) -> Int) {
 
     var tabla6 = Array<Int>(tamaño,rellenamiento)
+
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,6 +4,6 @@ import kotlin.random.nextInt
 fun main(args: Array<String>) {
 
     val tabla6 = ejercicicio2(10){ Random.nextInt(50..250)}
-    tabla6.tabla6.forEach { println(it) }
+    tabla6.tabla6.forEach { println(it)  }
 
 }
